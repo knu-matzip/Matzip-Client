cea3ddcca1d09f544d21acac0eadae31a6ee2884
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-17 14:24:33 +0900

    feat: BottomNavigation 및 TabItem 컴포넌트 추가

---
+++ apps/web/app/_components/BottomNavigation/BottomNavigation.tsx
@@ -0,0 +1,30 @@
+import { TabItem, type TabItemProps } from './TabItem'
+import { cn } from '@repo/ui/utils/cn'
+import { JustifyBetween } from '@repo/ui/components/Layout'
+
+const tabs: TabItemProps[] = [
+  { path: 'MAIN', label: '메인', icon: 'home' },
+  { path: 'MAP', label: '주변 맛집', icon: 'map' },
+  { path: 'PLACE_NEW', label: '', icon: 'circlePlus', iconSize: 50 },
+  { path: 'LIKES', label: '찜', icon: 'navHeart' },
+  { path: 'PROFILE', label: '내 정보', icon: 'navUser' },
+]
+
+export const BottomNavigation = () => {
+  return (
+    <JustifyBetween
+      as={'nav'}
+      className={cn(
+        'border-t-1',
+        'border-gray-50',
+        'h-15',
+        'mt-auto',
+        'px-5 py-2.5',
+      )}
+    >
+      {tabs.map((tab: TabItemProps) => (
+        <TabItem key={tab.path} {...tab} />
+      ))}
+    </JustifyBetween>
+  )
+}
---
+++ apps/web/app/_components/BottomNavigation/TabItem.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { CLIENT_PATH } from '@/_constants/path'
+import { cn } from '@repo/ui/utils/cn'
+import { Text } from '@repo/ui/components/Text'
+import { Icon, IconType } from '@repo/ui/components/Icon'
+
+export type TabItemProps = {
+  path: keyof Pick<
+    typeof CLIENT_PATH,
+    'MAIN' | 'MAP' | 'LIKES' | 'PROFILE' | 'PLACE_NEW'
+  >
+  icon: IconType
+  iconSize?: number
+  label?: string
+}
+
+export const TabItem = ({ path, label, icon, iconSize = 26 }: TabItemProps) => {
+  const pathname = usePathname()
+  const href = CLIENT_PATH[path]
+  const active = pathname === href
+
+  return (
+    <Link href={href} className={'w-12.5 flex flex-col items-center'}>
+      <Icon
+        type={icon}
+        size={iconSize}
+        color={active ? '--color-main' : '--color-gray-200'}
+      />
+      {label && (
+        <Text
+          fontWeight={'normal'}
+          className={cn('text-[10px]', active ? 'text-main' : 'text-gray-200')}
+        >
+          {label}
+        </Text>
+      )}
+    </Link>
+  )
+}
