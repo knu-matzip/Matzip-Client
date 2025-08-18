757c324eb1e515c927e25a9708d8fdff9f05a5f9
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-18 16:10:43 +0900

    feat: categories 하위 폴더 Template 컴포넌트 추가 - categories 하위 페이지 전환 시 animation effects 발생

---
+++ apps/web/app/_template/template.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { motion } from 'motion/react'
+import { ReactNode } from 'react'
+
+interface TemplateProps {
+  children: ReactNode
+}
+
+export default function Template({ children }: TemplateProps) {
+  return (
+    <motion.div
+      initial={{ x: 10, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: -10, opacity: 0 }}
+      transition={{
+        type: 'tween',
+        ease: 'easeOut',
+        duration: 0.3,
+      }}
+    >
+      {children}
+    </motion.div>
+  )
+}
