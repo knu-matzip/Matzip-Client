61aa776e8ae1973848c081445c017adec3c22454
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-22 00:25:04 +0900

    feat: 캠퍼스 위치 정보를 추가하여 CAMPUS_LOCATION 상수 정의

--- apps/web/app/_constants/campus.ts
+++ apps/web/app/_constants/campus.ts
@@ -4,5 +4,11 @@ export const CAMPUS = {
   yesan: '예산',
 } as const
 
+export const CAMPUS_LOCATION = {
+  singwan: { latitude: 36.469483428385914, longitude: 127.14059828594706 },
+  cheonan: { latitude: 36.85101236046876, longitude: 127.15093333537096 },
+  yesan: { latitude: 36.66990298154316, longitude: 126.85937468290652 },
+} as const
+
 export type CampusType = keyof typeof CAMPUS
 export const CAMPUS_LIST = Object.keys(CAMPUS) as CampusType[]
