c6556be1376e7a1e1dfe208588ae3d86fa4dd775
leeleeleeleejun <wnstjr6293@naver.com>
2025-09-29 21:59:51 +0900

    feat: addBaseUrl 함수를 별도 모듈로 추출

--- apps/web/app/_mocks/handlers/requestHandlers.ts
+++ apps/web/app/_mocks/handlers/requestHandlers.ts
@@ -1,12 +1,7 @@
 import { http, HttpResponse } from 'msw'
 import { API_PATH } from '@/_constants/path'
 import { requests } from '../data/request'
-
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || ''
-
-export const addBaseUrl = (path: string) => {
-  return `${BASE_URL}${path}`
-}
+import { addBaseUrl } from './addBaseUrl'
 
 export const RequestHandlers = [
   http.get(addBaseUrl(API_PATH.REQUEST.LIST), () => {
