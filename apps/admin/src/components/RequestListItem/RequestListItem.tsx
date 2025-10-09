b0b1db854de3d22737da7c291ee299d882c3a748
leeleeleeleejun <wnstjr6293@naver.com>
2025-10-09 14:57:29 +0900

    feat: RequestListItem 컴포넌트에 상세 페이지 이동 링크 추가 및 placeId 프로퍼티 추가

--- apps/admin/src/components/RequestListItem/RequestListItem.tsx
+++ apps/admin/src/components/RequestListItem/RequestListItem.tsx
@@ -1,27 +1,37 @@
 import { Column, Flex } from '@repo/ui/components/Layout'
 import { cn } from '@repo/ui/utils/cn'
 import { Text } from '@repo/ui/components/Text'
-import { Icon, IconType } from '@repo/ui/components/Icon'
+import { Icon, type IconType } from '@repo/ui/components/Icon'
+import { CLIENT_PATH } from '@/consts/path'
 
 export type Request = {
+  placeId: string
   placeName: string
   icon: IconType
   requestDate: string
 }
 
-export const RequestListItem = ({ placeName, icon, requestDate }: Request) => (
-  <Column
-    as={'li'}
-    className={cn('gap-1', 'py-3.5', 'border-b-1 border-gray-50')}
-  >
-    <Flex className={'gap-1'}>
-      <Text as={'span'} variant={'title2'}>
-        {placeName}
+export const RequestListItem = ({
+  placeId,
+  placeName,
+  icon,
+  requestDate,
+}: Request) => (
+  <li>
+    <Column
+      as={'a'}
+      href={CLIENT_PATH.REQUEST_DETAIL(placeId)}
+      className={cn('gap-1', 'py-3.5', 'border-b-1 border-gray-50')}
+    >
+      <Flex className={'gap-1'}>
+        <Text as={'span'} variant={'title2'}>
+          {placeName}
+        </Text>
+        <Icon type={icon} size={18} />
+      </Flex>
+      <Text variant={'caption2'} className={'text-gray-200'}>
+        등록 신청 일자: {requestDate}
       </Text>
-      <Icon type={icon} size={18} />
-    </Flex>
-    <Text variant={'caption2'} className={'text-gray-200'}>
-      등록 신청 일자: {requestDate}
-    </Text>
-  </Column>
+    </Column>
+  </li>
 )
