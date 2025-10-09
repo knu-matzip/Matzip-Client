ee1d10228044b7c1d0f8008a3a027c2e2d51ded6
leeleeleeleejun <wnstjr6293@naver.com>
2025-10-09 22:41:47 +0900

    feat: RequestsByCampus 컴포넌트에 MOCK_DATA를 사용하여 요청 목록 리팩토링 - placeId 필드 추가

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,36 +1,37 @@
 import { VerticalScrollArea } from '@repo/ui/components/Layout'
 import { RequestsByCampus } from '@/components/RequestsByCampus'
 import { OnlyLeftHeader } from '@repo/ui/components/Header'
+import { type Request } from '@/components/RequestListItem/RequestListItem'
+
+const MOCK_DATA: Request[] = [
+  {
+    placeId: '1',
+    placeName: '짬뽕집',
+    icon: 'chinese',
+    requestDate: '2025-10-29',
+  },
+  {
+    placeId: '2',
+    placeName: '짬뽕집',
+    icon: 'chinese',
+    requestDate: '2025-10-29',
+  },
+  {
+    placeId: '3',
+    placeName: '짬뽕집',
+    icon: 'chinese',
+    requestDate: '2025-10-29',
+  },
+]
 
 export default function Home() {
   return (
     <>
       <OnlyLeftHeader icon={'logo'} name={'대기중'} />
       <VerticalScrollArea className={'gap-10 p-5'}>
-        <RequestsByCampus
-          campus={'SINGWAN'}
-          requestList={[
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-          ]}
-        />
-        <RequestsByCampus
-          campus={'CHEANAN'}
-          requestList={[
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-          ]}
-        />
-        <RequestsByCampus
-          campus={'YESAN'}
-          requestList={[
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-            { placeName: '짬뽕집', icon: 'chinese', requestDate: '2025-10-29' },
-          ]}
-        />
+        <RequestsByCampus campus={'SINGWAN'} requestList={MOCK_DATA} />
+        <RequestsByCampus campus={'CHEANAN'} requestList={MOCK_DATA} />
+        <RequestsByCampus campus={'YESAN'} requestList={MOCK_DATA} />
       </VerticalScrollArea>
     </>
   )
