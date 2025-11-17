54ece42bc8c0d94bb76452a10d34d5ae6947dad6
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-17 21:26:44 +0900

    feat: 토큰 핸들링 및 에러 리디렉션을 위한 axiosInstance 요청/응답 인터셉터 추가 - api 요청 시 header에 accessToken 자동 삽입 (서버 / 클라이언트 요청 환경 구분)

--- apps/web/app/_lib/axiosInstance.ts
+++ apps/web/app/_lib/axiosInstance.ts
@@ -1,8 +1,45 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
+import { getCookie } from 'cookies-next'
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,
 })
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const isServer = typeof window === 'undefined'
+    let token: string | undefined
+
+    if (isServer) {
+      const { cookies } = await import('next/headers')
+      token = await getCookie('accessToken', { cookies })
+    } else {
+      token = await getCookie('accessToken')
+    }
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
+axiosInstance.interceptors.response.use(
+  // Todo: 재발급 로직 추가 필요
+
+  (response: AxiosResponse) => response.data,
+  (error) => {
+    if (error.response?.status === 401 && typeof window !== 'undefined') {
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  },
+)
 
 export default axiosInstance
