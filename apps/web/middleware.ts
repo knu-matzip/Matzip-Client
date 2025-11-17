3face6784e376815d910f60f60a262ed4c3e7cda
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-17 21:28:11 +0900

    feat: accessToken 유효성 검사 및 로그인 리디렉션을 위한 미들웨어 추가 - 인증이 필요한 페이지에서만 동작하도록 설정

---
+++ apps/web/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { CLIENT_PATH } from 'app/_constants/path'
+
+export async function middleware(request: NextRequest) {
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get('accessToken')?.value
+
+  if (!accessToken) {
+    return NextResponse.redirect(new URL(CLIENT_PATH.LOGIN, request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/likes',
+    '/profile',
+
+    '/places/new',
+    '/places/new/success',
+    '/places/new/fail',
+
+    '/requests',
+    '/requests/:path*',
+
+    '/events/lucky-draw',
+    '/events/gifticon',
+    '/events/gifticon/:path*',
+  ],
+}
