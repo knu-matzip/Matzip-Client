e311645bc9835201501fa2f4fd6c022c2dabd86b
leeleeleeleejun <wnstjr6293@naver.com>
2026-01-03 00:24:53 +0900

    fix: admin/next.config.ts 내 example.com 설정 제거

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -37,11 +37,6 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'example.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: process.env.NEXT_PUBLIC_API_URL_HOST || '', //테스트용 주소
         pathname: '/**',
       },
