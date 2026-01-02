615956f4ea142b1e6bc34c3ea43eb254fd1d293b
leeleeleeleejun <wnstjr6293@naver.com>
2026-01-02 23:50:44 +0900

    refactor: admin의 요청 상세페이지에서도 PlaceDetailPage와 같은 패턴 도입

--- apps/admin/src/app/requests/[id]/_components/Location/Location.tsx
+++ apps/admin/src/app/requests/[id]/_components/Location/Location.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import { Column } from '@repo/ui/components/Layout'
 import { Container, Marker, NaverMap } from 'react-naver-maps'
-import { Text } from '@repo/ui/components/Text'
 import { toLatLng } from '../../_utils/toLatLng'
 import type { RequestDetail } from '../../_api/types'
 
@@ -14,18 +12,15 @@ export const Location = ({ location }: Props) => {
   const setLocation = toLatLng(location)
 
   return (
-    <Column className={'gap-1.5'}>
-      <Text>위치</Text>
-      <Container className={'h-[150px] overflow-hidden rounded-xl'}>
-        <NaverMap
-          defaultZoom={18}
-          minZoom={15}
-          ref={setMap}
-          defaultCenter={setLocation}
-        >
-          <Marker position={setLocation} icon={'logo'} />
-        </NaverMap>
-      </Container>
-    </Column>
+    <Container className={'h-[150px] overflow-hidden rounded-xl'}>
+      <NaverMap
+        defaultZoom={18}
+        minZoom={15}
+        ref={setMap}
+        defaultCenter={setLocation}
+      >
+        <Marker position={setLocation} icon={'logo'} />
+      </NaverMap>
+    </Container>
   )
 }
--- apps/admin/src/app/requests/[id]/_components/Menus/Menus.tsx
+++ apps/admin/src/app/requests/[id]/_components/Menus/Menus.tsx
@@ -1,5 +1,5 @@
 import { Text } from '@repo/ui/components/Text'
-import { Column, VerticalScrollArea } from '@repo/ui/components/Layout'
+import { VerticalScrollArea } from '@repo/ui/components/Layout'
 import { cn } from '@repo/ui/utils/cn'
 import type { RequestDetail } from '@/app/requests/[id]/_api/types'
 import { Menu } from './Menu'
@@ -22,44 +22,41 @@ export const Menus = ({ menus }: Props) => {
   )
 
   return (
-    <Column className={'gap-2.5'}>
-      <Text>메뉴</Text>
-      <VerticalScrollArea
-        className={cn(
-          'gap-2.5',
-          'rounded-xl',
-          'bg-gray-50',
-          'px-4 py-3',
-          'max-h-60',
-          'show-scrollbar',
-        )}
-      >
-        {/*메뉴 존재 유무*/}
-        {menus.length === 0 && (
-          <Text
-            fontSize={'sm'}
-            fontWeight={'semibold'}
-            className={'mx-auto my-3'}
-          >
-            등록된 메뉴가 존재하지 않습니다
+    <VerticalScrollArea
+      className={cn(
+        'gap-2.5',
+        'rounded-xl',
+        'bg-gray-50',
+        'px-4 py-3',
+        'max-h-60',
+        'show-scrollbar',
+      )}
+    >
+      {/*메뉴 존재 유무*/}
+      {menus.length === 0 && (
+        <Text
+          fontSize={'sm'}
+          fontWeight={'semibold'}
+          className={'mx-auto my-3'}
+        >
+          등록된 메뉴가 존재하지 않습니다
+        </Text>
+      )}
+      {/*추천 메뉴 존재 유무*/}
+      {recommendedMenu.length > 0 && (
+        <>
+          <Text variant={'caption1'} className={'text-gray-300'}>
+            추천메뉴
           </Text>
-        )}
-        {/*추천 메뉴 존재 유무*/}
-        {recommendedMenu.length > 0 && (
-          <>
-            <Text variant={'caption1'} className={'text-gray-300'}>
-              추천메뉴
-            </Text>
-            {recommendedMenu.map((menu, index) => (
-              <Menu key={index} menu={menu} />
-            ))}
-            <hr className={'w-full border border-gray-200'} />
-          </>
-        )}
-        {unRecommendedMenu.map((menu, index) => (
-          <Menu key={index} menu={menu} />
-        ))}
-      </VerticalScrollArea>
-    </Column>
+          {recommendedMenu.map((menu, index) => (
+            <Menu key={index} menu={menu} />
+          ))}
+          <hr className={'w-full border border-gray-200'} />
+        </>
+      )}
+      {unRecommendedMenu.map((menu, index) => (
+        <Menu key={index} menu={menu} />
+      ))}
+    </VerticalScrollArea>
   )
 }
