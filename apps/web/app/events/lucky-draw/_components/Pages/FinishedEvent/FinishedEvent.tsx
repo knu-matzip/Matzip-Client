5c2883688f291287efc141d7ee98a7ecc9c76250
leeleeleeleejun <wnstjr6293@naver.com>
2026-02-17 23:16:31 +0900

    feat: 종료된 이벤트 목록에 EmptyFallback 컴포넌트 추가

--- apps/web/app/events/lucky-draw/_components/Pages/FinishedEvent/FinishedEvent.tsx
+++ apps/web/app/events/lucky-draw/_components/Pages/FinishedEvent/FinishedEvent.tsx
@@ -1,21 +1,35 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { CLIENT_PATH } from '@/_constants/path'
 import { Column, Flex } from '@repo/ui/components/Layout'
 import { Text } from '@repo/ui/components/Text'
+import { EmptyFallback } from '@/_components/EmptyFallback'
 
 export const FinishedEvent = () => {
   return (
-    <Column as={'ul'}>
-      <FinishedEventItem />
-      <FinishedEventItem />
-    </Column>
+    // Todo: EmptyFallback isEmpty 값 API 연동 후 동적으로 변경
+    <EmptyFallback
+      isEmpty={true}
+      fallbackTitle={'아직 종료된 이벤트가 없어요.'}
+      fallbackDescription={'진행 중인 이벤트에 참여하고 행운을 잡아보세요!'}
+    >
+      <Column as={'ul'}>
+        <FinishedEventItem />
+        <FinishedEventItem />
+      </Column>
+    </EmptyFallback>
   )
 }
 
 const FinishedEventItem = () => {
   return (
     <li className={'border-b-1 border-gray-100 py-3.5'}>
-      <Flex as={Link} href={''} className={'gap-2'}>
+      <Flex
+        as={Link}
+        // Todo: href 동적 라우팅으로 변경
+        href={CLIENT_PATH.EVENTS_LUCKY_DRAW + '/result/1'}
+        className={'gap-2'}
+      >
         <Image
           src={'/images/chicken.png'}
           alt={'종료된 이벤트 상품'}
