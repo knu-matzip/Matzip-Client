49f083bbfe338572984a6efeb2d2805db266d910
leeleeleeleejun <wnstjr6293@naver.com>
2026-02-26 14:36:44 +0900

    feat: 이벤트 엔트리 목록 mock 데이터 및 핸들러 추가

--- apps/web/app/_mocks/handlers/eventHandlers.ts
+++ apps/web/app/_mocks/handlers/eventHandlers.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse } from 'msw'
-import { eventResult } from '../data/event'
+import { EVENT_ENTRIES } from '../data/event'
 import { API_PATH } from '@/_constants/path'
 import { addBaseUrl } from './addBaseUrl'
 
@@ -7,10 +7,13 @@ export const EventHandlers = [
   // http.get(addBaseUrl(API_PATH.EVENT.INFO), () => {
   //   return HttpResponse.json(event)
   // }),
-  http.post(addBaseUrl(API_PATH.EVENT.PARTICIPATIONS), () => {
-    return HttpResponse.json({ message: '성공' })
-  }),
-  http.get(addBaseUrl('/events/:eventId/entries'), () => {
-    return HttpResponse.json(eventResult)
+  // http.post(addBaseUrl(API_PATH.EVENT.ENTRIES), () => {
+  //   return HttpResponse.json({ message: '성공' })
+  // }),
+  // http.get(addBaseUrl('/events/:eventId/entries'), () => {
+  //   return HttpResponse.json(eventResult)
+  // }),
+  http.get(addBaseUrl(API_PATH.EVENT.ENTRIES), () => {
+    return HttpResponse.json(EVENT_ENTRIES)
   }),
 ]
